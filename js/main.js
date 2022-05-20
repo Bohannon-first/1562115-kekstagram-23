a4bdf037495a7e4a6014087ee2f39811624b9a73
Ярослав <spartacus215@yandex.ru>
2022-05-20 17:42:30 +0300

    Выполнил раздел Больше деталей

--- js/main.js
+++ js/main.js
@@ -38,12 +38,6 @@ getRandomNumber();
 // Количество необходимых объектов для генерации
 const QUANTITY_DESCRIPTIONS_PHOTOS = 25;
 
-// Идентификатор описания
-const ID_DESCR = {
-  MIN: 1,
-  MAX: 25,
-};
-
 // Список описаний фотографий
 const DESCRIPTIONS = [
   'Описание 1-й фотографии',
@@ -83,7 +77,13 @@ const LIKES = {
 // Идентификатор комментария
 const ID_COMMENT = {
   MIN: 1,
-  MAX: 1000000,
+  MAX: 1000,
+};
+
+// Количество аватарок пользователей
+const AVATARS = {
+  MIN: 1,
+  MAX: 6,
 };
 
 // Список комментариев к фотографиям
@@ -125,20 +125,36 @@ const NAMES = [
   'Гурген',
 ];
 
-// Пустой массив идентификаторов описания
+// Массив идентификаторов описания
 const arrayId = [];
-
-// Пустой массив с картинками
-const arrayPhotos = [];
 
 // Счётчик заполнения пустого массива идентификаторами описаний
 for (let idItemCounter = 1; idItemCounter <= QUANTITY_DESCRIPTIONS_PHOTOS; idItemCounter++) {
   arrayId.push(idItemCounter);
 }
 
+// Массив с картинками
+const arrayPhotos = [];
+
 // Счётчик заполнения пустого массива картинками
 for (let photoItemCounter = 1; photoItemCounter <= QUANTITY_DESCRIPTIONS_PHOTOS; photoItemCounter++) {
   arrayPhotos.push(`photos/${photoItemCounter}.jpg`);
+}
+
+// Массив ID комментариев к фотографиям
+const arrayIdComments = [];
+
+// Счётчик заполнения пустого массива ID комментариев к фоткам
+for (let idItemCounter = ID_COMMENT.MIN; idItemCounter <= ID_COMMENT.MAX; idItemCounter++) {
+  arrayIdComments.push(idItemCounter);
+}
+
+// Массив аватаров
+const arrayAvatars = [];
+
+// Счётчик заполнения массива аватаров
+for (let avatarItemCounter = AVATARS.MIN; avatarItemCounter <= AVATARS.MAX; avatarItemCounter++) {
+  arrayAvatars.push(`img/avatar-${avatarItemCounter}.svg`);
 }
 
 // Вырезание/удаление случайного элемента из заполненного массива
@@ -146,14 +162,46 @@ const cutRandomElementArray = function (someArray) {
   return someArray.splice(getRandomNumber(0, someArray.length - 1), 1);
 };
 
+// Получение рандомного элемента из массива
+const getRandomElementArray = function (someArray) {
+  return someArray[getRandomNumber(0, someArray.length - 1)];
+};
+
+// Функция генерации комментариев к фотографиям
+const createComments = function () {
+  return {
+    id: `${cutRandomElementArray(arrayIdComments)}`,
+    avatar: `${getRandomElementArray(arrayAvatars)}`,
+    message: `${getRandomElementArray(MESSAGES)}`,
+    name: `${getRandomElementArray(NAMES)}`,
+  };
+};
+
+// Массив с комментариями к фотке
+const arrayComments = [];
+
+// Функция получения списка комментариев к фотке
+const getListComments = function () {
+  const numberComments = getRandomNumber(AVATARS.MIN, AVATARS.MAX);
+
+  for (let comment = 1; comment <= numberComments; comment++) {
+    arrayComments.push(createComments());
+  }
+
+  return arrayComments.splice(0);
+};
 
 // Функция генерации объектов
 const createPhotos = function () {
   return {
     id: `${cutRandomElementArray(arrayId)}`,
     url: `${cutRandomElementArray(arrayPhotos)}`,
+    description: `${cutRandomElementArray(DESCRIPTIONS)}`,
+    likes: `${getRandomNumber(LIKES.MIN, LIKES.MAX)}`,
+    comments: getListComments(),
   };
 };
 
 const listPhotos = new Array(QUANTITY_DESCRIPTIONS_PHOTOS).fill(null).map(() => createPhotos());
-console.log(listPhotos);
+listPhotos;
+// console.log(listPhotos);
